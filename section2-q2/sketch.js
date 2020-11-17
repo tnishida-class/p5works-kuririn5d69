523de68e83f27aede967dc49540851c6e3c6ec53
kuririn5d69 <65203343+kuririn5d69@users.noreply.github.com>
2020-11-17 10:33:04 +0900

    2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,9 +3,35 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
+  fill(128,128,128);
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(i % 2 == 0) {
+      rect(i*25, j*50+25, 25, 25);
+      if(j<1) {
+        fill(255,0,0);
+        ellipse(i*25+12.5, j*50+37.5, 20);
+        fill(128,128,128);
+      }
+      if(j>1) {
+        fill(0,0,0);
+        ellipse(i*25+12.5, j*50+37.5, 20);
+        fill(128,128,128);
+      }
+      }
+    else{
+      rect(i*25, j*50, 25, 25);
+      if(j<2) {
+        fill(255,0,0);
+        ellipse(i*25+12.5, j*50+12.5, 20);
+        fill(128,128,128);
+      }
+      if(j>2) {
+        fill(0,0,0);
+        ellipse(i*25+12.5, j*50+12.5, 20);
+        fill(128,128,128);
+      }
     }
   }
+}
 }
