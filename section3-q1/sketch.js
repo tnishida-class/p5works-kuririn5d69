60999dc8a23dcd7eec4ba37c2b2cee447ce293c1
kuririn5d69 <65203343+kuririn5d69@users.noreply.github.com>
2020-11-24 08:56:20 +0900

    3-q1 re

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -13,17 +13,9 @@ function balloon(t){
   let p = 100;
   noStroke();
 
-  fill(180, 99, 165);
-  arc(w + p * 1.3 + 25, h + p * 1.5 + 15, p, p / 2, QUARTER_PI, PI);
-  ellipse(w/2 + p + 25, h/2 + p + 15, w + p * 1.5, h + p * 1.5);
-
-  fill(255);
-  arc(w + p * 1.3 + 2, h + p * 1.5 - 2, p + 12, p / 2 + 12, QUARTER_PI, PI);
-  ellipse(w/2 + p, h/2 + p, w + p * 1.5 + 10, h + p * 1.5 + 10);
-
-  fill(95, 185, 85);
+  fill(255,10,0);
   arc(w + p * 1.3, h + p * 1.5, p, p / 2, QUARTER_PI, PI);
   ellipse(w/2 + p, h/2 + p, w + p * 1.5, h + p * 1.5);
-  fill(255);
+  fill(0);
   text(t, p, h + p);
 }
