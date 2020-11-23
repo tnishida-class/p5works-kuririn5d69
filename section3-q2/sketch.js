5c0b973dd8acab628d55943217581d53e18320d1
kuririn5d69 <65203343+kuririn5d69@users.noreply.github.com>
2020-11-23 21:21:24 +0900

    3-q2

--- section3-q2/sketch.js
+++ section3-q2/sketch.js
@@ -12,7 +12,7 @@ function kobeCity(x, y, size){
   strokeWeight(size * 0.25);
   strokeCap(SQUARE);
   arc(x + size * 0.25, y, size, size, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  arc(x - size * 0.25, y, size, size, QUARTER_PI * 3 + PI / 2, QUARTER_PI * 3 - PI / 2);
   pop();
 }
 
@@ -22,4 +22,16 @@ function kobeUniv(x, y, size){
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+  push();
+  noStroke();
+  fill(blue);
+  ellipse(x + size * 0.25, y, size);
+  fill(255);
+  ellipse(x + size * 0.25 - 6, y - 6, size - 7.5);
+  fill(green);
+  ellipse(x + size * 0.25 - 7, y - 7, size - 15);
+  fill(255);
+  ellipse(x + size * 0.25 - 10, y + 1, size - 20);
+  //赤いやつの書き方分からないです、お手上げです。;
+  pop();
 }
