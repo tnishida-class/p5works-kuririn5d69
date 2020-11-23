f85d46cccbe01d54a988e01e6208682c3a019733
kuririn5d69 <65203343+kuririn5d69@users.noreply.github.com>
2020-11-23 13:43:04 +0900

    3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -10,9 +10,20 @@ function setup(){
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = 100;
+  noStroke();
+
+  fill(180, 99, 165);
+  arc(w + p * 1.3 + 25, h + p * 1.5 + 15, p, p / 2, QUARTER_PI, PI);
+  ellipse(w/2 + p + 25, h/2 + p + 15, w + p * 1.5, h + p * 1.5);
+
+  fill(255);
+  arc(w + p * 1.3 + 2, h + p * 1.5 - 2, p + 12, p / 2 + 12, QUARTER_PI, PI);
+  ellipse(w/2 + p, h/2 + p, w + p * 1.5 + 10, h + p * 1.5 + 10);
+
+  fill(95, 185, 85);
+  arc(w + p * 1.3, h + p * 1.5, p, p / 2, QUARTER_PI, PI);
+  ellipse(w/2 + p, h/2 + p, w + p * 1.5, h + p * 1.5);
   fill(255);
   text(t, p, h + p);
 }
